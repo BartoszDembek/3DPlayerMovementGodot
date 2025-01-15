e697e0c6100dd79bdbfd35215130f76f5ab92cdf
BartoszDembek <bartoszdembek944@gmail.com>
2025-01-15 19:19:18 +0100

    Change Camera scene type, change Camera script, add wall to Level scene

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -9,5 +9,6 @@ var movement_input := Vector2.ZERO
 func _physics_process(_delta: float) -> void:
 	movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera.global_rotation.y)
 	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
+	
 	move_and_slide()
 	
